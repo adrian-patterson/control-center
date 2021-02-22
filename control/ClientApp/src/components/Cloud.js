be5c81ccf3ea2e015fc17c9212c8c98471e1d699
canadrian72 <patterson.n.adrian@gmail.com>
2021-02-22 16:03:42 -0500

    Personalized home page and changed component names

--- control/ClientApp/src/components/Cloud.js
+++ control/ClientApp/src/components/Cloud.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 
-export class Counter extends Component {
-  static displayName = Counter.name;
+export class Cloud extends Component {
+  static displayName = Cloud.name;
 
   constructor(props) {
     super(props);
     this.state = { currentCount: 0 };
-    this.incrementCounter = this.incrementCounter.bind(this);
+    this.incrementCloud = this.incrementCloud.bind(this);
   }
 
-  incrementCounter() {
+  incrementCloud() {
     this.setState({
       currentCount: this.state.currentCount + 1
     });
@@ -18,13 +18,13 @@ export class Counter extends Component {
   render() {
     return (
       <div>
-        <h1>Counter</h1>
+        <h1>Cloud</h1>
 
         <p>This is a simple example of a React component.</p>
 
         <p aria-live="polite">Current count: <strong>{this.state.currentCount}</strong></p>
 
-        <button className="btn btn-primary" onClick={this.incrementCounter}>Increment</button>
+        <button className="btn btn-primary" onClick={this.incrementCloud}>Increment</button>
       </div>
     );
   }
