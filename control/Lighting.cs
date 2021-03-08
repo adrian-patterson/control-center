40298836b9af37a9d49670b43d2137609eb3cdb3
Julian Patterson <julianedwardpatterson@gmail.com>
2021-03-07 23:07:14 -0500

    Julian's first commit

--- control/Lighting.cs
+++ control/Lighting.cs
@@ -3,13 +3,16 @@ using System.Device.Gpio;
 
 namespace control
 {
-	public class Lighting
-	{
+    public class Lighting
+    {
         GpioController controller = new GpioController();
         private static readonly int pin = 23;
-		public string toggle { get; set; }
+        public string toggle { get; set; }
+        public int r { get; set; }
+        public int g { get; set; }
+        public int b { get; set; }
 
-		public Lighting()
+        public Lighting()
 		{
             controller.OpenPin(pin, PinMode.Output);
         }
@@ -25,5 +28,14 @@ namespace control
             controller.Write(pin, PinValue.Low);
             Console.WriteLine("LED off");
 		}
+
+        public void setRgb()
+        {
+            // ws281.setColor(r,g,b)
+            // you're garb
+            // you're going to have to use Color objects to set them
+            // Color myColor = new Color(r,g,b)
+            // also will have to import color library 'using System.Color'
+        }
 	}
 }
